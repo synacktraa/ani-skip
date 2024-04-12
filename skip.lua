de2382f48aeee3ab255cf5fab18996830fd8fc57
Woreec <46332697+Woreec@users.noreply.github.com>
2024-04-12 11:45:41 +0200

    Feat: Keyboard shortcut to disable lua script

--- skip.lua
+++ skip.lua
@@ -2,6 +2,9 @@
 local mpv = require('mp')
 local mpv_options = require("mp.options")
 
+--Default states
+local enabled = true
+local keybind = "i"
 local options = { -- setting default options
     op_start = 0, op_end = 0, ed_start = 0, ed_end = 0,
 }
@@ -11,10 +14,14 @@ mpv_options.read_options(options, "skip") --reading script-opts data
 local function skip()
     local current_time = mp.get_property_number("time-pos")
     
+    if not enabled then  
+        return
+    end
+    
     if not current_time then
         return
     end
-
+    
     -- Check for opening sequence
     if current_time >= options.op_start and current_time < options.op_end then
         mp.set_property_number("time-pos", options.op_end)
@@ -26,5 +33,11 @@ local function skip()
     end
 end
 
+local function enabled_state()
+    enabled = not enabled
+    mp.osd_message("Ani-Skip is " .. (enabled and "enabled" or "disabled"), 1)
+end  
+
 -- Bind the function to be called whenever the time position is changed
 mp.observe_property("time-pos", "number", skip)
+mp.add_key_binding("i", "skipping", enabled_state)
